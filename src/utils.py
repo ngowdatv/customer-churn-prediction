efcac48223055cf769d682f901dac7b5a357e3cf
nesara <ngowdatv@gmail.com>
2026-07-08 15:17:01 +0530

    Added utility functions

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import dill
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    """
+    Save any Python object (model, preprocessor, etc.) using dill.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    """
+    Load a saved Python object.
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
