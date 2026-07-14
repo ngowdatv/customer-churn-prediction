f509af9c9bb2c5e6ee5c6ec0922d05d88b4bcd1d
nesara <ngowdatv@gmail.com>
2026-07-14 10:36:45 +0530

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 8000
 
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "flask_app:app"]
+CMD ["uvicorn","main:app","--host","0.0.0.0","--port","8000"]
--- main.py
+++ main.py
@@ -3,62 +3,76 @@ from pydantic import BaseModel
 import pandas as pd
 import joblib
 
+
 app = FastAPI(
     title="Customer Churn Prediction API",
     description="Predict whether a customer will churn",
     version="1.0"
 )
 
-# Load trained model
+
+# Load model
 model = joblib.load("model.pkl")
 columns = joblib.load("columns.pkl")
 
 
-# Input Schema
+# Input parameters shown in Swagger
 class ChurnInput(BaseModel):
+
     gender: str
     SeniorCitizen: int
     Partner: str
     Dependents: str
+
     tenure: int
-    PhoneService: str
-    MultipleLines: str
+
     InternetService: str
-    OnlineSecurity: str
-    OnlineBackup: str
-    DeviceProtection: str
-    TechSupport: str
-    StreamingTV: str
-    StreamingMovies: str
     Contract: str
-    PaperlessBilling: str
-    PaymentMethod: str
+
     MonthlyCharges: float
     TotalCharges: float
+
+    PaymentMethod: str
+
 
 
 @app.get("/")
 def home():
-    return {"message": "Customer Churn Prediction API Running 🚀"}
+
+    return {
+        "message": "Customer Churn Prediction API Running 🚀"
+    }
+
 
 
 @app.post("/predict")
 def predict(data: ChurnInput):
 
-    input_df = pd.DataFrame([data.model_dump()])
+    # Convert input to dataframe
+    input_df = pd.DataFrame(
+        [data.model_dump()]
+    )
 
-    # One-hot encoding
+
+    # Encoding
     input_df = pd.get_dummies(input_df)
 
-    # Match training columns
-    input_df = input_df.reindex(columns=columns, fill_value=0)
+
+    # Match model training columns
+    input_df = input_df.reindex(
+        columns=columns,
+        fill_value=0
+    )
+
 
     # Prediction
     prediction = model.predict(input_df)[0]
 
+
     result = "Churn" if prediction == 1 else "No Churn"
+
 
     return {
         "Prediction": result,
-        "Prediction Value": int(prediction)
+        "Prediction_Value": int(prediction)
     }
